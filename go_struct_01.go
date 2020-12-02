0f052cdb6e71677f4d3351b10c5488c105d31843
naye02 <vvv9247@naver.com>
2020-12-02 21:55:46 +0900

    v3 struct

--- go_struct_01.go
+++ go_struct_01.go
@@ -6,14 +6,22 @@ type part struct {
 	description string
 	count       int
 }
-
 func showInfo(p part) {
 	fmt.Println("Description : ", p.description)
 	fmt.Println("Count : ", p.count)
 }
+func minimumOrder(description string) part{
+	var p part
+	p.description = description
+	p.count = 1000
+	return p
+}
+
 func main() {
-	var bolts part
-	bolts.description = "Hex bolts"
-	bolts.count = 100
-	showInfo(bolts)
+	p:=minimumOrder("Hex bolts")
+	fmt.Println(p.description, p.count)
+//	var bolts part
+//	bolts.description = "Hex bolts"
+//	bolts.count = 100
+//	showInfo(bolts)
 }
