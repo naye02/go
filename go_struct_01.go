5ff79727b4fdb42d78dd996cd97e72145f8062bc
naye02 <vvv9247@naver.com>
2020-12-02 21:42:20 +0900

    version2

---
+++ go_struct_01.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type part struct {
+	description string
+	count       int
+}
+
+func showInfo(p part) {
+	fmt.Println("Description : ", p.description)
+	fmt.Println("Count : ", p.count)
+}
+func main() {
+	var bolts part
+	bolts.description = "Hex bolts"
+	bolts.count = 100
+	showInfo(bolts)
+}
