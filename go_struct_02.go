f40b694f0207c5467a1a20c394aefb40db5c14e0
naye02 <vvv9247@naver.com>
2020-12-02 22:33:06 +0900

    v1 struct

--- go_struct_02.go
+++ go_struct_02.go
@@ -7,12 +7,13 @@ type subscriber struct {
 	rate   int
 	active bool
 }
+
 func printInfo(s subscriber) {
-	fmt.Println("Name : ",s.name)
-	fmt.Println("Monthly rate : ",s.rate)
-	fmt.Println("Active? : ",s.active)
+	fmt.Println("Name : ", s.name)
+	fmt.Println("Monthly rate : ", s.rate)
+	fmt.Println("Active? : ", s.active)
 }
-func defaultSubscriber(name string)subscriber {
+func defaultSubscriber(name string) subscriber {
 	var s subscriber
 	s.name = name
 	s.rate = 5000
@@ -23,7 +24,7 @@ func main() {
 	s1 := defaultSubscriber("Kim")
 	s1.rate = 4500
 	printInfo(s1)
-	
+
 	s2 := defaultSubscriber("Park")
 	printInfo(s2)
 }
